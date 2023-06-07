860f40724c8943b788c090a52659e726998adb0f
Nick Douglas <nick.douglas82@gmail.com>
2023-06-07 17:41:48 +0100

    Initial creation

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,118 +1,134 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Home() {
-  return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    return (
+        <div className="bg-white">
+            <div className="relative bg-gray-900">
+                <div aria-hidden="true" className="absolute inset-0 overflow-hidden">
+                    <Image
+                        src="/images/edgar-castrejon-CX8ooha2yLA-unsplash-min.jpg"
+                        width={3242}
+                        height={2161}
+                        alt="Hero image"
+                    />
+                </div>
+                <div
+                    aria-hidden="true"
+                    className="absolute inset-0 bg-gray-900 opacity-50"
+                />
+
+                <div
+                    className="relative flex flex-col items-center max-w-3xl px-6 py-32 mx-auto text-center sm:py-64 lg:px-0"
+                >
+                    <h1
+                        className="text-4xl font-bold tracking-tight text-white lg:text-6xl"
+                    >
+                        An e-commerce demo site
+                    </h1>
+                    <p className="mt-4 text-xl text-white">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Aspernatur vel, modi, ducimus ut a accusamus id quis magnam
+                        maxime natus earum suscipit obcaecati nisi aperiam. Cumque
+                        accusamus quasi molestias incidunt?
+                    </p>
+                    <Link
+                        href="#"
+                        className="inline-block px-8 py-3 mt-8 text-base font-medium text-gray-900 bg-white border border-transparent rounded-md hover:bg-gray-100"
+                    >Shop New Arrivals
+                    </Link>
+                </div>
+            </div>
+
+            <main>
+                <section
+                    aria-labelledby="collection-heading"
+                    className="max-w-xl px-4 pt-24 mx-auto sm:px-6 sm:pt-32 lg:max-w-7xl lg:px-8"
+                >
+                    <h2
+                        id="collection-heading"
+                        className="text-2xl font-bold tracking-tight text-gray-900"
+                    >
+                        Shop by Collection
+                    </h2>
+                    <p className="mt-4 text-base text-gray-500">
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                        Culpa adipisci provident labore molestiae placeat sunt
+                        facere itaque magni quis delectus laboriosam fugiat minus
+                        mollitia reprehenderit voluptas unde nam, dicta debitis!
+                    </p>
+
+                    <div
+                        className="mt-10 space-y-12 lg:grid lg:grid-cols-3 lg:gap-x-8 lg:space-y-0"
+                    >
+                        {/* <NuxtLink
+                        v-for="collection in collections"
+                        :key="collection.name"
+                        :to="`collections/${collection.slug}`"
+                        className="block group"
+                    >
+                        <div
+                            aria-hidden="true"
+                            className="overflow-hidden rounded-lg aspect-h-2 aspect-w-3 lg:aspect-h-6 lg:aspect-w-5 group-hover:opacity-75"
+                        >
+                            <img
+                                :src="collection.img"
+                                :alt="collection.name"
+                                className="object-cover object-center w-full h-full"
+                            />
+                        </div>
+                        <h3 className="mt-4 text-base font-semibold text-gray-900">
+                            {{ collection.name }}
+                        </h3>
+                        <p className="mt-2 text-sm text-gray-500">
+                            {{ collection.description }}
+                        </p>
+                    </NuxtLink> */}
+                    </div>
+                </section>
+
+                <section
+                    aria-labelledby="comfort-heading"
+                    className="px-4 py-24 mx-auto max-w-7xl sm:px-6 sm:py-32 lg:px-8"
+                >
+                    <div className="relative overflow-hidden rounded-lg">
+                        <div className="absolute inset-0">
+                            <Image
+                                src="/images/sidekix-media--iQYdCr4EpE-unsplash-min.jpg"
+                                width={1500}
+                                height={1000}
+                                alt="Call to action image"
+                            />
+                        </div>
+                        <div
+                            className="relative px-6 py-32 bg-gray-900 bg-opacity-75 sm:px-12 sm:py-40 lg:px-16"
+                        >
+                            <div
+                                className="relative flex flex-col items-center max-w-3xl mx-auto text-center"
+                            >
+                                <h2
+                                    id="comfort-heading"
+                                    className="text-3xl font-bold tracking-tight text-white sm:text-4xl"
+                                >
+                                    Some whitty title
+                                </h2>
+                                <p className="mt-3 text-xl text-white">
+                                    Lorem, ipsum dolor sit amet consectetur
+                                    adipisicing elit. Esse autem inventore harum
+                                    similique excepturi, tenetur nesciunt eos alias
+                                    tempora non beatae dolore. Maxime nobis minus
+                                    earum molestiae impedit, cumque ratione.
+                                </p>
+                                <Link
+                                    href="#"
+                                    className="block w-full px-8 py-3 mt-8 text-base font-medium text-gray-900 bg-white border border-transparent rounded-md hover:bg-gray-100 sm:w-auto"
+                                >A none working button
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </main>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    )
 }
